bc7551f1b819fd99b7f29777425dcc862380b28b
Tom Virgo <tomcvirgo@gmail.com>
2021-08-16 14:37:46 +0100

    add socket-io client and create connection to backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Landing } from './pages';
+const io = require('socket.io-client');
+const ENDPOINT = 'http://localhost:3000';
 
 const App = () => {
-  return <Landing />;
+  const [socket, setSocket] = useState();
+
+  useEffect(()=>{
+    const newSocket = io(ENDPOINT);
+    setSocket(newSocket);
+  }, [])
+  // const socket = io(ENDPOINT);
+
+  return (<Landing socket={socket} />);
 };
 
 export default App;
