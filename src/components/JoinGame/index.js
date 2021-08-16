1557dfd190d4fb78f458506a6b3bf7ba88a9b3fb
akashkhambay <akashkhambay@hotmail.co.uk>
2021-08-16 11:50:10 +0100

    Add handle form submission and input updating functions

--- src/components/JoinGame/index.js
+++ src/components/JoinGame/index.js
@@ -2,14 +2,26 @@ import React from "react";
 
 const JoinGame = () => {
   const [username, setUsername] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDeafult();
+    setUsername("");
+  };
+
+  const updateInput = (e) => {
+    const input = e.target.value;
+    setUsername(input);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleFormSubmit}>
       <label htmlFor="username"> Enter username</label>
       <input
         type="text"
         id="username"
         name="username"
         placeholder="Please enter a username"
+        onChange={updateInput}
       />
       <input type="submit" value="Submit" />
     </form>
