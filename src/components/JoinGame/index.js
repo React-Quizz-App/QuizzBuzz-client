826c53b0b6ae7e8fb9006bae78f38aee03cb1e97
akashkhambay <akashkhambay@hotmail.co.uk>
2021-08-16 11:28:42 +0100

    Add username form in joingame component

---
+++ src/components/JoinGame/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const JoinGame = () => {
+  return (
+    <form>
+      <label htmlFor="username"> Enter username</label>
+      <input
+        type="text"
+        id="username"
+        name="username"
+        placeholder="Please enter a username"
+      />
+    </form>
+  );
+};
