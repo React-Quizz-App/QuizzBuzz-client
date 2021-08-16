a29d0dccd269254b01e03715922e94c7eca4cd2d
Rafika <RTuremis@hotmail.co.uk>
2021-08-16 11:00:03 +0100

    Setup baasic structure for landing page

---
+++ src/pages/Landing/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      <button>Highscores</button>
+      <div>
+        <h2>QuizzBuzz</h2>
+      </div>
+      <button>Create Game</button>
+      <button>Join Game</button>
+    </div>
+  );
+};
+
+export default Landing;
