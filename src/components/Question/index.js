974553ea625b6da24a14979716fe786434600512
Tom Virgo <tomcvirgo@gmail.com>
2021-08-17 18:20:02 +0100

    add countdown timer

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,17 +1,35 @@
 import React, { useState, useEffect } from 'react';
 import { incrementQuestionNumber, updateScore } from '../../actions';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 
-const Question = ({ handleAnswer, data: { question, correct_answer, incorrect_answers } }) => {
+const Question = ({ data: { question, correct_answer, incorrect_answers } }) => {
 
+  const [counter, setCounter] = useState(10);
   const [shuffledAnswers, setShuffledAnswers] = useState([]);
   const [selectedOption, setSelectedOption] = useState('');
   const dispatch = useDispatch();
+  const gameState = useSelector(state => state.gameState);
+  const socket = useSelector(state => state.socket);
+  const clientUser = useSelector(state => state.user);
 
   useEffect(()=>{
     setShuffledAnswers([correct_answer, ...incorrect_answers].sort(() => Math.random() - 0.5))
   }, [question])
+
+  useEffect(() => {
+    function decrementCounter(){
+      setCounter(prevCount => prevCount - 1);
+    }
+    decrementCounter();
+    const stream = setInterval(()=>decrementCounter(), 1000)
+    return () => clearInterval(stream)
+  }, [question]);
+
+  if (counter === 0 ){
+    dispatch(incrementQuestionNumber())
+    setCounter(10);
+  };
 
   function handleChange(event){
     setSelectedOption(event.target.value);
@@ -22,17 +40,26 @@ const Question = ({ handleAnswer, data: { question, correct_answer, incorrect_an
     event.preventDefault();
     console.log(selectedOption);
     // increment question number by one
-    dispatch(incrementQuestionNumber())
     // if score is correct update the score
-    if (selectedOption === correct_answer){
+    if (selectedOption === correct_answer && gameState.questionNumber <= 10){
       dispatch(updateScore());
+      // let newState = {...gameState};
+      // let newUsers = [...gameState.users];
+      // let userIdx = newUsers.findIndex(item => item.name === clientUser);
+      // console.log(userIdx);
+      // newUsers[userIdx].score += 1;
+      // newState.users = newUsers;
+      // console.log(newState);
+      // socket.emit('send state to players', newState);
     };
+
   }
   return (
     <div>
       <div>
         <h2>{question}</h2>
       </div>
+      <p>{counter}</p>
       <div>
         <form onSubmit={handleSubmit}>
         {shuffledAnswers.map((answer) => (
