c651e408f12c00b3772aa6771b867a07bee5935f
akashkhambay <akashkhambay@hotmail.co.uk>
2021-08-17 12:04:09 +0100

    Create button component to help clean up code

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 // Making Questions page component to be rendered on game page
+const Button = ({ answer }) => <button>{answer}</button>;
 const Question = ({
   data: { question, correct_answer, incorrect_answers },
 }) => {
-  console.log(incorrect_answers);
   return (
     <div>
       <div>
         <h2>{question}</h2>
       </div>
       <div>
-        <button>{correct_answer}</button>
-        <button>{incorrect_answers[0]}</button>
-        <button>{incorrect_answers[1]}</button>
-        <button>{incorrect_answers[2]}</button>
+        <Button answer={correct_answer} />
+        <Button answer={incorrect_answers[0]} />
+        <Button answer={incorrect_answers[1]} />
+        <Button answer={incorrect_answers[2]} />
       </div>
     </div>
   );
