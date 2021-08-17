c06f1a468e09302edbebcf75aa774439a4608b6b
akashkhambay <akashkhambay@hotmail.co.uk>
2021-08-17 10:44:43 +0100

    Add placeholder text for button and questions values

--- src/components/Question/index.js
+++ src/components/Question/index.js
@@ -2,6 +2,14 @@ import React from "react";
 // Making Questions page component to be rendered on game page
 const Quuestion = () => (
   <div>
-    <div></div>
+    <div>
+      <h2>Here is where the question will go</h2>
+    </div>
+    <div>
+      <button>Answer1</button>
+      <button>Answer2</button>
+      <button>Answer3</button>
+      <button>Answer4</button>
+    </div>
   </div>
 );
