c99705815823162f0341f1277bc9b621fde8c952
Tom Virgo <tomcvirgo@gmail.com>
2021-08-17 14:18:53 +0100

    integrate waiting room with redux store

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ const App = () => {
       </Route>
     </Switch>
   );
-
+  }
 
 
 export default App;
--- src/pages/WaitingRoom/index.js
+++ src/pages/WaitingRoom/index.js
@@ -3,26 +3,13 @@ import { useSelector } from 'react-redux';
 import './style.css';
 
 const WaitingRoom = () => {
-    
-    let data = { gamestate : {
-                                roomName : "zlcqum",
-                                category: "Entertainment Film",
-                                difficulty: "hard",
-                                host: "Tom",
-                                users: [
-                                    {name: "Ben", score: 0},
-                                    {name: "Rafika", score: 0},
-                                    {name: "Akash", score: 0},
-                                    {name: "Jawwad", score: 0},
-                                    {name: "Ben", score: 0}
-                                ],
-                                questionNumber: 1,
-                                socket: {},
-                                user: "Tom"
-    }};
 
-    return ( 
-        <div className="waiting-room-page">
+    const data = useSelector(state => state.gameState);
+    const user = useSelector(state => state.user);
+    console.log('waiting room data: ', data);
+    return (
+        <> 
+        {data.users && <div className="waiting-room-page">
             <header>
                 <div>
                     <h1>QUIZZBUZZ</h1>
@@ -31,30 +18,42 @@ const WaitingRoom = () => {
             <div className="waiting-room-outer-container">
                 <div className="waiting-room-inner">
                     <div className="waiting-message">
-                        <h1>This is {data.gamestate.host}'s room </h1>
+                        <h1>This is {data.host}'s room </h1>
                         <h2>Please wait for other players...</h2>
                     </div>
                     <div className="player-section">
-                    {data.gamestate.users.map(user => {
+                    {data.users.map(user => {
                         return <div className="player-card" key={user.name}> <h3> {user.name} </h3> </div>
                     })}
                     </div>
-                    {data.gamestate.host === data.gamestate.user ? <div className="start-section">
+                    {data.host === user ? <div className="start-section">
                                                                         <div className="game-link">
                                                                             <div className="game-link-left">
                                                                                 <p>Share room name to friends:</p>
                                                                             </div>
                                                                             <div className="game-link-right">
-                                                                                 <span>{data.gamestate.roomName}</span>
+                                                                                 <span>{data.roomName}</span>
                                                                             </div>
                                                                         </div>
                                                                         <button className="start-game">START GAME</button>
                                                                     </div> 
-                                                                    : null}
+                                                                    : 
+                                                                    <div className="start-section">
+                                                                        <div className="game-link">
+                                                                            <div className="game-link-left">
+                                                                                <p>Share room name to friends:</p>
+                                                                            </div>
+                                                                            <div className="game-link-right">
+                                                                                 <span>{data.roomName}</span>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div> 
+                                                                    }
                 </div>
             </div>
             {console.log(data)}
-        </div>
+        </div>}
+        </>
      );
 }
  
