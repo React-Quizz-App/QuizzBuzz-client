e704aa2f46b3c0170a16ddbe7ed45eae463909de
akashkhambay <akashkhambay@hotmail.co.uk>
2021-08-17 11:03:57 +0100

    Dummy rendering for test purposes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { Landing } from './pages';
-import { useDispatch } from 'react-redux';
-const io = require('socket.io-client');
-const ENDPOINT = 'http://localhost:3000';
-import { createGame, storeSocket } from './actions';
+// import React, { useEffect, useState } from 'react';
+// import { Landing } from './pages';
+// import { useDispatch } from 'react-redux';
+// const io = require('socket.io-client');
+// const ENDPOINT = 'http://localhost:3000';
+// import { createGame, storeSocket } from './actions';
+
+// const App = () => {
+
+//   const dispatch = useDispatch();
+
+//   useEffect(() => {
+//     const newSocket = io(ENDPOINT);
+//     dispatch(storeSocket(newSocket));
+//     newSocket.on('init state', (state)=>{
+//       dispatch(createGame(state));
+//     });
+//   }, []);
+
+//   return (<Landing />);
+// };
+
+// export default App;
+import React from "react";
+import { Game } from "./pages";
 
 const App = () => {
-  
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const newSocket = io(ENDPOINT);
-    dispatch(storeSocket(newSocket));
-    newSocket.on('init state', (state)=>{
-      dispatch(createGame(state));
-    });
-  }, []);
-
-  return (<Landing />);
+  return <Game />;
 };
 
 export default App;
