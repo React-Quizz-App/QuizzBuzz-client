02028368b95b24829ce2a668aa027808483a3188
Rafika <RTuremis@hotmail.co.uk>
2021-08-18 09:02:15 +0100

    Start HighScores page

--- src/App.js
+++ src/App.js
@@ -1,56 +1,57 @@
 import React, { useEffect, useState } from 'react';
-import { Landing, WaitingRoom, Game } from './pages';
+import { Landing, WaitingRoom, Game, HighScores } from './pages';
 import { useDispatch, useSelector } from 'react-redux';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom';
 const io = require('socket.io-client');
 const ENDPOINT = 'http://localhost:3000';
 import { changeState, storeSocket, addUser } from './actions';
 
 const App = () => {
-  
   const [socket, setSocket] = useState();
   const dispatch = useDispatch();
-  const clientUser = useSelector(state=>state.user);
-  const host = useSelector(state => state.gameState.host);
-  const gameState = useSelector(state => state.gameState);
+  const clientUser = useSelector((state) => state.user);
+  const host = useSelector((state) => state.gameState.host);
+  const gameState = useSelector((state) => state.gameState);
 
   // initialise a socket and events to listen for
-  useEffect(()=>{
+  useEffect(() => {
     const newSocket = io(ENDPOINT);
     dispatch(storeSocket(newSocket));
-    newSocket.on('change state', (state)=>{
+    newSocket.on('change state', (state) => {
       dispatch(changeState(state));
     });
     setSocket(newSocket);
   }, []);
 
-
-  useEffect(()=>{
-    if (socket){
+  useEffect(() => {
+    if (socket) {
       socket.on('user joining waiting room', (user) => {
-        if (clientUser === host){
-          dispatch(addUser(user))
-          let newGameState = {...gameState};
-          newGameState.users.push({name: user, score: 0});
+        if (clientUser === host) {
+          dispatch(addUser(user));
+          let newGameState = { ...gameState };
+          newGameState.users.push({ name: user, score: 0 });
           socket.emit('send state to players', newGameState);
         }
-      })
+      });
     }
-  }, [socket, clientUser, host])
+  }, [socket, clientUser, host]);
 
   return (
     <Switch>
-      <Route exact path='/'>
+      <Route exact path="/">
         <Landing />
       </Route>
-      <Route path='/waiting-room'>
+      <Route path="/waiting-room">
         <WaitingRoom />
       </Route>
-      <Route path='/game'>
+      <Route path="/game">
         <Game />
+      </Route>
+      <Route path="/highscores">
+        <HighScores />
       </Route>
     </Switch>
   );
-  }
+};
 
 export default App;
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,20 +1,25 @@
-import React, { useState } from "react";
-import { CreateGame, JoinGame } from "../../components";
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { HighScores } from '../../pages';
+import { CreateGame, JoinGame } from '../../components';
 
 const Landing = () => {
   const [isFormShown, setIsFormShown] = useState(false);
   const [isJoinFormShown, setIsJoinFormShown] = useState(false);
+  const [isHighscoresShown, setIsHighscoresShown] = useState(false); //this is an intermediate solution, should use NavLink
 
   const toggleForm = () => setIsFormShown((prev) => !prev);
   const toggleJoinForm = () => setIsJoinFormShown((prev) => !prev);
+  const toggleHighscores = () => setIsHighscoresShown((prev) => !prev);
   return (
     <div>
-      <button>Highscores</button>
+      <button onClick={toggleHighscores}>Highscores</button>
       <div>
         <h2>QuizzBuzz</h2>
       </div>
       <button onClick={toggleForm}>Create Game</button>
       <button onClick={toggleJoinForm}>Join Game</button>
+      {isHighscoresShown && <HighScores />}
       {isFormShown && <CreateGame />}
       {isJoinFormShown && <JoinGame />}
     </div>
