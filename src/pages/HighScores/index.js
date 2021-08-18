6fa1cd4ac7cb319ff610247af878950e27105626
Rafika <RTuremis@hotmail.co.uk>
2021-08-18 18:00:10 +0100

    Highscores works and redirects

--- src/pages/HighScores/index.js
+++ src/pages/HighScores/index.js
@@ -43,9 +43,9 @@ const HighScores = () => {
 
   let renderHighscores = saveSortedHighScores.map((s, i) => (
     <div key={i}>
-      <p>{s.rank}</p>
-      <p>{s.username}</p>
-      <p>{s.score}</p>
+      <p className="rank">{s.rank}</p>
+      <p className="userName">{s.username}</p>
+      <p className="userScore">{s.score}</p>
     </div>
   ));
 
@@ -64,28 +64,25 @@ const HighScores = () => {
 
   return (
     <div id="highscores">
-      {isFilterSelected ? (
-        renderHighscores
-      ) : (
-        <form onSubmit={handleSubmit}>
-          <select name="Category" id="category-filter" onChange={handleCategoryFilter} required>
-            <option value="placeholder">By Category</option>
-            <option value="General Knowledge">General Knowledge</option>
-            <option value="Entertainment: Books">Entertainment: Books</option>
-            <option value="Entertainment: Film">Entertainment: Film</option>
-            <option value="Entertainment: Music">Entertainment: Music</option>
-            <option value="sports">Sports</option>
-            <option value="Science: Computers">Science: Computers</option>
-          </select>
-          <select name="Difficulty" id="difficulty-filter" onChange={handleDifficultyFilter} required>
-            <option value="placeholder-for-difficulty">Difficulty</option>
-            <option value="easy">Easy</option>
-            <option value="medium">Medium</option>
-            <option value="hard">Hard</option>
-          </select>
-          <input type="submit" value="Filter Results" />
-        </form>
-      )}
+      <form onSubmit={handleSubmit}>
+        <select name="Category" id="category-filter" onChange={handleCategoryFilter} required>
+          <option value="placeholder">By Category</option>
+          <option value="General Knowledge">General Knowledge</option>
+          <option value="Entertainment: Books">Entertainment: Books</option>
+          <option value="gaming">Entertainment: Film</option>
+          <option value="Entertainment: Music">Entertainment: Music</option>
+          <option value="sports">Sports</option>
+          <option value="Science: Computers">Science: Computers</option>
+        </select>
+        <select name="Difficulty" id="difficulty-filter" onChange={handleDifficultyFilter} required>
+          <option value="placeholder-for-difficulty">Difficulty</option>
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+        <input type="submit" value="Filter Results" />
+      </form>
+      {isFilterSelected ? renderHighscores : <p></p>}
     </div>
   );
 };
